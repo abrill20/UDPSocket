42ddf4ac1bde125db7c0dec8562b398c848591f2
Aaron Brill <abrill@skidmore.edu>
2019-04-05 13:21:22 -0400

    Add main method

--- UDPClient.py
+++ UDPClient.py
@@ -1,22 +1,25 @@
-from socket import *
+import socket
 import time, sys
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.settimeout(1)
-for i in range(0,10):
-  message = input('Input lowercase sentence: ')
-  clientSocket.sendto(message.encode(), (serverName, serverPort))
-  startTime = time.time()
-  sequence_number = i
-  try: 
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    endTime = time.time() - startTime
-    print(modifiedMessage.decode(), "RTT: ",  endTime, sequence_number)
-  except:
-    print("Request Timed Out")
-      
+def Main():
+  host = '127.0.0.1'
+  port = 12000
+  with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as clientSocket:
+    clientSocket.settimeout(1)
+    for i in range(0,10):
+      message = input('Input lowercase sentence: ')
+      clientSocket.sendto(message.encode(), (host, port))
+      startTime = time.time()
+      sequence_number = i
+      try: 
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        endTime = time.time() - startTime
+        print(modifiedMessage.decode(), "RTT: ",  endTime, sequence_number)
+      except:
+        print("Request Timed Out")
+          
+    clientSocket.close()
+    sys.exit()
 
-clientSocket.close()
-sys.exit()
+if __name__ == '__main__':
+  Main()
--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -1,23 +1,27 @@
 # UDPPingerServer.py
 # We will need the following module to generate randomized lost packets
 import random
-from socket import *
+import socket
 
-# CreateaUDP socket
-# Notice the use of SOCKDGRAM for UDP packets
-serverSocket = socket (AF_INET , SOCK_DGRAM)
-# Assign IP address and port number to socket
-serverSocket.bind(('',12000))
-while True :
-  print('Ready to ping...')
-  # Generate random number in the range of0to 10
-  rand = random.randint(0,10)
-  # Receive the client packetalong with the address it is coming from
-  message, address = serverSocket.recvfrom(1024)
-  # Capitalize the message from the client
-  message = message.upper()
-  # If rand is less than 3, we consider the packet lost and do not respond
-  if rand < 3:
-    continue
-  # Otherwise, the server responds
-  serverSocket.sendto(message, address)
+def Main():
+  # CreateaUDP socket
+  # Notice the use of SOCKDGRAM for UDP packets
+  with socket.socket(socket.AF_INET , socket.SOCK_DGRAM) as serverSocket:
+    # Assign IP address and port number to socket
+    serverSocket.bind(('',12000))
+    while True :
+      print('Ready to ping...')
+      # Generate random number in the range of0to 10
+      rand = random.randint(0,10)
+      # Receive the client packetalong with the address it is coming from
+      message, address = serverSocket.recvfrom(1024)
+      # Capitalize the message from the client
+      message = message.upper()
+      # If rand is less than 3, we consider the packet lost and do not respond
+      if rand < 3:
+        continue
+      # Otherwise, the server responds
+      serverSocket.sendto(message, address)
+
+if __name__ == '__main__':
+  Main()
